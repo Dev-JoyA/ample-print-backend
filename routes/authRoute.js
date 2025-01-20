725b700a2a57ce7c0140949401d23af4df4b6ff2
Joy A <joy.gold13@gmail.com>
2025-01-20 18:41:34 +0100

    Admin auth endoint done

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,12 +1,15 @@
 import express, {Router} from "express";
-// import passport from "passport";
 import passport from "../middleware/passport.js"
 import { signIn, signUp } from "../controllers/authControllers.js";
+import { adminSignIn, adminSignUp , deleteAdmin} from "../controllers/adminAuthController.js"
 
 const router = Router();
 
 router.post("/sign-in", signIn)
 router.post("/sign-up", signUp)
+router.post("/admin/sign-in", adminSignIn)
+router.post("/admin/sign-up", adminSignUp)
+router.delete("/delete/admin", deleteAdmin)
 router.get("/google",  passport.authenticate('google', { scope:
     [ 'email', 'profile' ] }
 ));
