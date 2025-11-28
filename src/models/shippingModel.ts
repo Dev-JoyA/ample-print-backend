ab8fc4b8240bd04de75b831e59f4c9c5a2a0b618
Joy A <joy.gold13@gmail.com>
2025-11-28 15:40:28 +0100

    updated service folder and added models for paayment

---
+++ src/models/shippingModel.ts
@@ -0,0 +1,49 @@
+import {Document , model, Types, Schema} from "mongoose";
+
+export enum ShippingMethod {
+    Pickup = "pickup",
+    Delivery = "delivery"
+}
+
+export enum ShippingStatus {
+    Pending = "pending",
+    Shipped = "shipped",
+    Delivered = "delivered"
+}
+
+export interface IShipping extends Document {
+    orderId : Types.ObjectId;
+    shippingMethod : ShippingMethod;
+    trackingNumber : string;
+    status : ShippingStatus;
+}
+
+const ShippingSchema = new Schema<IShipping>(
+    {
+        orderId: {
+            type: Schema.Types.ObjectId,
+            ref: "Order",
+            required: true,
+        },
+        shippingMethod: {
+            type: String,
+            enum: Object.values(ShippingMethod),
+            required: true
+        },
+        trackingNumber: {
+            type: String,
+            required: true
+        },
+        status: {
+            type: String,
+            enum: Object.values(ShippingStatus),
+            default: ShippingStatus.Pending,
+            required: true
+        }
+    },
+    { timestamps: true }
+);
+
+export const Shipping = model<IShipping>("Shipping", ShippingSchema);
+
+
---
+++ src/service/orderService.ts
@@ -0,0 +1,7 @@
+export const createOrder = async () => {};
+export const updateOrder = async () => {};
+export const deleteOrder = async() => {};
+export const trackOrder = async() => {};
+export const getOrderById = async() => {};
+export const getUserOrder = async() => {};
+export const filterOrder = async () => {};
